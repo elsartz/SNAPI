ec7cf485a85e1a126a5265ac0eb2979838747631
VSartz <elsartz@gmail.com>
2022-06-18 14:06:21 -0400

    just thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,11 +1,46 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-    getAllThoughts() {},
-    getThoughtById() {},
-    createThought() {},
-    updateThought() {},
-    deleteThought() {},
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.status(400).json(err));    
+    },
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .select('-__v')
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'Thought not found' });
+                }
+                res.json(dbThoughtData);
+            })
+    },
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.status(400).json(err))
+    },
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params._id }, body, {new: true, runValidators: true})
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: 'Thought not found' });
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    return res.status(404).json({ message: 'Thought not found' });
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));},
     createReaction() {},
     deleteReaction() {}
 }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,7 +12,7 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
     getUserById({ params }, res) {
-        User.findOne({ _id: params.id})
+        User.findOne({ _id: params.id })
             .populate({
                 path: 'thoughts',
                 select: '-__v'
