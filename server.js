3b6b3abc83f19f42ec247579e813b0eea4c4e834
VSartz <elsartz@gmail.com>
2022-06-18 19:14:24 -0400

    path to controlers corrected

--- server.js
+++ server.js
@@ -14,5 +14,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/snapi_db'
   useUnifiedTopology: true
 });
 
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
 
 app.listen(PORT, () => console.log(`🌎 Connected on localhost:${PORT}`));
